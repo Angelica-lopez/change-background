1de73b0e82a1730512322cd1d96cc1889c2eb299
Angelica <angielopez4004@gmail.com>
2021-04-22 16:37:04 -0500

    color button text

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ const change = document.querySelector(".content__item2");
 button.addEventListener('click', function() {
     const number = Math.floor(Math.random()*12);
     document.body.style.backgroundColor = colors[number];
+    button.style.color = colors[number];
+    change.style.color = colors[number];
     change.textContent = colors[number];
 })
 
 button2.addEventListener('click', function() {
     document.body.style.backgroundColor = "#f1f5f8";
     change.textContent = "#f1f5f8";
+    button.style.color = "#f1f5f8";
+    change.style.color = "#f1f5f8";
 })
