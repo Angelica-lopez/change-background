eed96acbd233fba7b40f8cf6ed9059ad20fc9105
Angelica <angielopez4004@gmail.com>
2021-04-22 15:28:36 -0500

    button reset

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const colors = ["#E8DAEF", "#6FEBFC", "#6FFCAF", "#E0FC6F", "#FF55FC", "#B96483", "#3460E8", "#73FFD7", "#F9C112", "#2F6321", "#90A99D", "#5D375E"];
 const button = document.querySelector(".button");
+const button2 = document.querySelector(".button--2");
 const change = document.querySelector(".content__item2");
 
 
@@ -9,3 +10,7 @@ button.addEventListener('click', function() {
     change.textContent = colors[number];
 })
 
+button2.addEventListener('click', function() {
+    document.body.style.backgroundColor = "#f1f5f8";
+    change.textContent = "#f1f5f8";
+})
